b12a0a23b05f542cf6dc4b6baab9994bff08aa1e
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-06-04 00:26:44 +0100

    creation of grid layoiut and implementation of the css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Best from "./components/Best/Best";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <Best />
     </div>
   );
 }
